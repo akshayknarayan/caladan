aaf01ccc6acec171f7c2110c38d31d07cf7dc780
Akshay Narayan <akshayn@mit.edu>
2020-12-01 17:57:14 +0000

    fix setup_machine script

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -18,10 +18,9 @@ chmod uga+rwx /dev/ksched
 # reserve huge pages
 echo 5192 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 echo 0 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
-for n in /sys/devices/system/node/node[2-9]; do
+for n in /sys/devices/system/node/node{2..9}; do
 	echo 0 > $n/hugepages/hugepages-2048kB/nr_hugepages
 done
 
 # load msr module
 modprobe msr
-
